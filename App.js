582e0d69c27c6b717d6989d2c9ea42d8539d2771
p8ul <pkinuthia10@gmail.com>
2021-04-26 07:19:03 +0300

    updated react-native-google-signin version

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { mapping } from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
 import { Provider as ReduxProvider } from 'react-redux';
 import RootNavigator from './src/navigator/RootNavigator';
 import { store } from './src/store';
-import { setupHttpConfig } from './src/utils/http';
 import { crowdboticsTheme } from './src/config/crowdboticsTheme';
 import { default as customMapping } from './src/config/mapping.json';
 
 export default function App() {
-  useEffect(() => {
-    setupHttpConfig();
-  }, []);
-
   return (
     <ReduxProvider store={store}>
       <ApplicationProvider
--- src/config/app.js
+++ src/config/app.js
@@ -1,3 +1,3 @@
 export const appConfig = {
-  backendAPIEndPoint: 'http://192.168.0.101:8000/',
+  backendAPIEndPoint: 'https://yenne-app-25688.botics.co/', // 'http://192.168.0.101:8000/',
 };
