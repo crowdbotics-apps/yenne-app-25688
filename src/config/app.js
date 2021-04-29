237c811781d7b7786217a93ecae0f7d8e017b70c
p8ul <pkinuthia10@gmail.com>
2021-04-29 12:48:49 +0300

    updated backend api endpoint

--- src/config/app.js
+++ src/config/app.js
@@ -1,4 +1,3 @@
 export const appConfig = {
-  backendAPIEndPoint: 'http://192.168.0.100:8000/',
-  // backendAPIEndPoint: 'https://yenne-app-25688.botics.co/', // 'http://192.168.0.101:8000/',
+  backendAPIEndPoint: 'https://yenne-app-25688.botics.co/', // 'http://192.168.0.101:8000/',
 };
