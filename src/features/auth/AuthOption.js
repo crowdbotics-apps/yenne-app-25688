621be9d4150ead2e6ae7cd0403218cab8023a48c
p8ul <pkinuthia10@gmail.com>
2021-05-12 07:37:11 +0300

    pupdated faouth2 api keys

--- src/features/auth/AuthOption.js
+++ src/features/auth/AuthOption.js
@@ -45,14 +45,14 @@ const Icon = ({ name }) => {
 GoogleSignin.configure({
   scopes: ['email', 'profile'], // what API you want to access on behalf of the user, default is email and profile
   webClientId:
-    '445580192151-ejtuvcr2sc1spm5k4p7igb6h4vq0sego.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+    '4459140409-btn1nusljjd6s8i2q5ijh35h5ueeib8e.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
   offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
   hostedDomain: '', // specifies a hosted domain restriction
   loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
   forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
   accountName: '', // [Android] specifies an account name on the device that should be used
   iosClientId:
-    '1019078873013-qeemg0n9p4bu10ujgupnkiev6ja1ed5r.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    '4459140409-629nbi4cavcvpalgkmv8t00v4bajsm22.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
   googleServicePlistPath: '', // [iOS] optional, if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
 });
 
